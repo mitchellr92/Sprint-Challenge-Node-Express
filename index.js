fdd258821452ef8ef590cf734fd09263e58c205f
Mitchell Robles <mitchellr92@live.com>
2018-12-08 09:24:30 -0800

    set up very basic index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const server = express();
+const PORT = 1010;
+
+server.get()
+
+server.listen(PORT, () => {
+    console.log(`Server is live on port ${PORT}`)
+})
