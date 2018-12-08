d51105047addc03fdebc1671bd320cc4fe6270fd
Mitchell Robles <mitchellr92@live.com>
2018-12-08 10:05:39 -0800

    created get request for individual id and post request

--- index.js
+++ index.js
@@ -4,14 +4,55 @@ const projectDb = require('./data/helpers/projectModel.js')
 const server = express();
 const PORT = 1010;
 
+server.use(express.json());
+
 server.get('/projects', (req, res) => {
     projectDb.get()
         .then(projects => {
             res.json(projects)
         })
+        // Did not work without anonymous function
         .catch(err => {
             res.status(500).json({ message: 'Failed to load projects' })
         })
+})
+
+server.get('/projects/:id', (req, res) => {
+
+    const { id } = req.params;
+
+    projectDb.get(id)
+        .then(project => {
+            if (project) {
+                res.json(project)
+            } else {
+                res.status(404).json({ message: 'Porject with specified ID does not exoist' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to load project' })
+        })
+})
+
+server.post('/projects', (req, res) => {
+
+    const project = req.body;
+
+    if (project.name && project.description) {
+        projectDb.insert(project)
+            .then(idInfo => {
+                projectDb.get(idInfo.id).then(project => {
+                    res.status(201).json(project)
+                })
+            })
+            .catch(err => {
+                res.status(500).json({message: 'Failed to insert project'})
+            })
+    } else {
+        res.status(400).json({
+            message: 'Missing name or description'
+        })
+    }
 })
 
 server.listen(PORT, () => {
