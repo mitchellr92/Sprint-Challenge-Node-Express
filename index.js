215e6b1507e407a0e81c5f5c4ac373d9588742ad
Mitchell Robles <mitchellr92@live.com>
2018-12-08 09:52:28 -0800

    created get request for projects

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const projectDb = require('./data/helpers/projectModel.js')
 
 const server = express();
 const PORT = 1010;
 
-server.get()
+server.get('/projects', (req, res) => {
+    projectDb.get()
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to load projects' })
+        })
+})
 
 server.listen(PORT, () => {
     console.log(`Server is live on port ${PORT}`)
